9efeb97e783d6adca4eb6bb9c282923b90d318d6
Luminescq <tarpy.socdev@gmail.com>
2026-02-26 15:12:49 +0300

    Add bot configuration settings
    Add configuration settings for the bot including token, admin IDs, and update interval.

---
+++ bot/config.py
@@ -0,0 +1,6 @@
+# config.py
+# Все настройки бота
+
+BOT_TOKEN = "7851126415:AAFxhrhvacSXYlEXV7iREyiM9RhF1WIrY7w"
+ADMIN_IDS = [5805064246]  # сюда свои ID
+UPDATE_INTERVAL = 300  # обновление статуса каждые 5 минут
